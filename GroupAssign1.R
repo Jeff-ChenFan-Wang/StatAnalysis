837ef35ad6700f15e405158ae7871e6b2c411b8e
sarmadafzalj <112432975+sarmadafzalj@users.noreply.github.com>
2022-10-07 12:05:26 -0500

    updated
    Heat map + (Normality, heteroskedasticity, serial correlation)

--- GroupAssign1.R
+++ GroupAssign1.R
@@ -12,6 +12,13 @@ df <- get_acs(
 df<- df[grep("M$",names(df),invert=TRUE)]
 colnames(df) <- c('geoid','name','totpop','medage','medhhinc','propbac','propcov','proppov','proprent','geometry')
 
+#heat map
+ggplot() + 
+geom_sf(data = df, aes(fill = propbac), show.legend = TRUE)
++ ggtitle("Heatmap of Baccalaureate Attainment Rate in the Cook County")
++ scale_fill_viridis_c(option="B", direction = -1)
+
+
 linAlg <- lm('propbac ~ medhhinc',data=df)
 summary(linAlg)
 
@@ -30,3 +37,20 @@ abline(linAlg,
 grid()
 
 summary(linAlg)
+
+# Finding Normality, Serial Correlation, heteroskedasticity
+# Normality
+hist(linAlg$residuals)
+plot(linAlg$residuals)
+ks.test(linAlg$residuals, pnorm) #test and plots show that the data is not normal
+
+#Serial Correlation, need to discuss during meeting
+dwtest(linAlg) # There is serial correlation we will reject Null hypothesis, shows serial corr..
+plot(acf(linAlg$residuals, type = "correlation")) # does not show serial corr
+plot(df$propbac[-1319], df$propbac[-1]) #lag and current values plot - does not show serial corr
+
+#Heteroskedasticity
+plot(linAlg) #look for residual vs Fitted plot
+bptest(linAlg) #shows hetero..
+
+
