ae73b8060dd477b9f9968545affee74371657a42
Chen Tao <taochen8365@gmail.com>
2022-10-13 21:22:41 +0000

    Chen Tao: Finish step 10 and 11, and start step 12

--- GroupAssign1.R
+++ GroupAssign1.R
@@ -19,9 +19,9 @@ colnames(df) <- c('geoid','name','totpop','medage','medhhinc','propbac','propcov
 
 #heat map
 ggplot() +
-geom_sf(data = df, aes(fill = propbac), show.legend = TRUE) +
-ggtitle(label = "Heatmap of Baccalaureate Attainment Rate in the Cook County") +
-scale_fill_viridis_c(name = 'Proportion', option="B", direction = -1)
+  geom_sf(data = df, aes(fill = propbac), show.legend = TRUE) +
+  ggtitle(label = "Heatmap of Baccalaureate Attainment Rate in the Cook County") +
+  scale_fill_viridis_c(name = 'Proportion', option="B", direction = -1)
 
 
 linAlg <- lm('propbac ~ medhhinc',data=df)
@@ -71,4 +71,45 @@ plot(density(CorSample), col = "blue",
      main = "The distribution of correlations",
      xlab = "Correlation",lty = 1, lwd = 1.5)
 
+
+### Week 3 ###
+# Step 10
+inter = linAlg$coefficients['(Intercept)']
+slope = linAlg$coefficients['medhhinc']
+slope_test = seq(0, 10*slope, 0.01*slope)
+SSE_Opt = sum(linAlg$residuals^2)
+SSE = sapply(slope_test, function(x) sum((x*df$medhhinc + inter - df$propbac)^2, na.rm = TRUE))
+plot(slope_test, SSE, type = 'l', col = 'blue',
+     main = 'Changes in MSE for different slopes on median household income')
+points(x = slope, y = SSE_Opt, col = 'red', pch = 16)
+abline(v = slope, col = 'red', lty = 2)
+
+# Step 11
+inter_test = seq(0, 5*inter, 0.01*inter)
+data = data.frame(cbind(df$medhhinc, df$propbac)) %>%
+  rename(X = X1, Y = X2) %>% drop_na()
+log_likelihood = function(inter, slope. = slope, x = data$X, y = data$Y){
+  SSE = sum((y - slope.*x - inter)^2); n = length(y)
+  sigma_sq = SSE/(n - 2)
+  return(-(log(sigma_sq)/2 + log(2*pi)/2)*n - SSE/(2*sigma_sq))
+}
+log_likeli = sapply(inter_test, log_likelihood)
+plot(inter_test, log_likeli, type = 'l', col = 'blue',
+     main = 'Changes in log-likelihood for different intercepts')
+points(x = inter, y = log_likelihood(inter), col = 'red', pch = 16)
+abline(v = inter, col = 'red', lty = 2)
+
+# Step 12
+sum(df$totpop*df$propbac/100, na.rm = TRUE)
+df_new = df %>% drop_na(medhhinc, propbac)
+df_new[['medhhinc_policy']] = df_new[['medhhinc']]
+medhhinc_sort = sort(df_new[['medhhinc']], index.return = TRUE)$ix
+df_new[['medhhinc_policy']][medhhinc_sort[1:50]] = df_new[['medhhinc_policy']][medhhinc_sort[1:50]] + 10000
+df_new[['medhhinc_policy']][rev(medhhinc_sort)[1:50]] = df_new[['medhhinc_policy']][rev(medhhinc_sort)[1:50]] - 10000
+df_new[['propbac_pre']] = slope*df_new[['medhhinc_policy']] + inter
+sum(df_new$totpop*df_new$propbac_pre/100)
+ggplot() +
+  geom_sf(data = df_new, aes(fill = propbac_pre), show.legend = TRUE) +
+  ggtitle(label = "Heatmap of Predicted Baccalaureate Attainment Rate") +
+  scale_fill_viridis_c(name = 'Proportion', option="B", direction = -1)
 
